27938fa8e9ae1e93e2fa30551824d1d8333f9ac9
Taonga Mtonga <taomtonga7@gmail.com>
2025-08-18 21:26:53 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const http = require('http');
 
 const app = require('./app');
 
+console.log('JWT KEY:', process.env.JWT_KEY);
+
 const port = process.env.port || 3000;
 
 const server = http.createServer(app);
